d5b5c38a8e3e647345e2eef8b6024540bc375049
carlalo <timo@goeckeler.com>
2019-11-08 16:50:41 +0100

    Stupid code to let the computer run out of processing power.

---
+++ Unlimited/src/Unlimitid.java
@@ -0,0 +1,13 @@
+
+public class Unlimitid {
+	public static void main(String[] args) {
+		for (int i = 1; i < 101; i++) {
+			System.out.println(i);
+			if ((i / 2) * 2 == i) {
+				System.out.println("Achtung! Diese Nummer ist durch 2 teilbar");
+			}
+
+		}
+	}
+
+}
